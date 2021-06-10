9d7d11e1077ad6ef568a7812f7ffd150bfa0a904
Ali Burzhi <aliburzhi033@gmail.com>
2021-06-10 13:57:46 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-
+import React from 'react';
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
+
+const link = React.createElement('a', { href: 'reactjs.org' }, 'Ссылка');
+
+console.log(link);
